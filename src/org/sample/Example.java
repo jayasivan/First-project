dc2c7214a72d0c676f030318d0e82fca34c7ec84
jayasivan <jaisivamech@gmail.com>
2022-01-27 09:40:39 +0530

    iuytr

---
+++ src/org/sample/Example.java
@@ -0,0 +1,9 @@
+package org.sample;
+
+public class Example {
+public static void main(String[] args) {
+	System.out.println("siva");
+	System.out.println("venki");
+	System.out.println("bharathi");
+}
+}
